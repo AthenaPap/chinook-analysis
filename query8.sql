4ce88b5df23016ac45556a0d2310373cf13e967e
AthenaPap <85991640+AthenaPap@users.noreply.github.com>
2021-06-16 14:34:13 +0300

    Delete query8.sql

--- query8.sql
+++
@@ -1,4 +0,0 @@
-SELECT E.lastname as EmployeeName, E.birthdate as EmployeeBirthday, M.lastname as ManagerName, M.birthdate as ManagerBirthday FROM EMPLOYEE E
-INNER JOIN EMPLOYEE M 
-ON e.reportsTo = m.employeeId
-AND e.birthdate < m.birthdate
